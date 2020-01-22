e690655ed46d4177e0b938a947f10904c7d37379
adrian <adrianmircor@gmail.com>
2020-01-22 17:24:07 -0500

    modificacion2

--- FISI/Debug/src/FISI.d
+++
@@ -1,6 +0,0 @@
-src/FISI.o: ../src/FISI.c /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
- /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
-
-/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
-
-/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
